2c3f64c6bedf7d87f543920bd1d9245593e6046a
majd217 <m.amireh@gju.edu.jo>
2024-04-05 12:34:13 +0300

    PickFiles library implemented

--- lib/upload.dart
+++ lib/upload.dart
@@ -1,4 +1,6 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class UploadYourPhotos extends StatefulWidget {
@@ -7,12 +9,87 @@ class UploadYourPhotos extends StatefulWidget {
 }
 
 class UploadYourPhotosState extends State<UploadYourPhotos> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+  final _defaultFileNameController = TextEditingController();
+  final _dialogTitleController = TextEditingController();
+  final _initialDirectoryController = TextEditingController();
+  final _fileExtensionController = TextEditingController();
+  String? _fileName;
+  String? _saveAsFileName;
+  List<PlatformFile>? _paths;
+  String? _directoryPath;
+  String? _extension;
+  bool _isLoading = false;
+  bool _lockParentWindow = false;
+  bool _userAborted = false;
+
+  void _logException(String message) {
+    print(message);
+    _scaffoldMessengerKey.currentState?.hideCurrentSnackBar();
+    _scaffoldMessengerKey.currentState?.showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _resetState() {
+    if (!mounted) {
+      return;
+    }
+    setState(() {
+      _isLoading = true;
+      _directoryPath = null;
+      _fileName = null;
+      _paths = null;
+      _saveAsFileName = null;
+      _userAborted = false;
+    });
+  }
+
+  void _pickFiles() async {
+    _resetState();
+    try {
+      _directoryPath = null;
+      _paths = (await FilePicker.platform.pickFiles(
+        compressionQuality: 30,
+        type: FileType.media,
+        allowMultiple: true,
+        onFileLoading: (FilePickerStatus status) => print(status),
+        allowedExtensions: (_extension?.isNotEmpty ?? false)
+            ? _extension?.replaceAll(' ', '').split(',')
+            : null,
+        dialogTitle: _dialogTitleController.text,
+        initialDirectory: _initialDirectoryController.text,
+        lockParentWindow: _lockParentWindow,
+      ))
+          ?.files;
+    } on PlatformException catch (e) {
+      _logException('Unsupported operation' + e.toString());
+    } catch (e) {
+      _logException(e.toString());
+    }
+    if (!mounted) return;
+    setState(() {
+      _isLoading = false;
+      _fileName =
+          _paths != null ? _paths!.map((e) => e.name).toString() : '...';
+      _userAborted = _paths == null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
           width: double.infinity,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               image: DecorationImage(
                   opacity: .8,
                   image: AssetImage('assets/images/background.JPG'),
@@ -20,31 +97,31 @@ class UploadYourPhotosState extends State<UploadYourPhotos> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 100,
               ),
               Container(
-                padding: EdgeInsets.all(15),
+                padding: const EdgeInsets.all(15),
                 width: MediaQuery.of(context).size.width / 1.1,
                 height: MediaQuery.of(context).size.height / 8,
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.6),
+                    borderRadius: const BorderRadius.all(Radius.circular(15))),
                 child: Center(
                   child: Text(
                       style: TextStyle(
                           fontStyle: FontStyle.italic,
                           fontFamily: GoogleFonts.greatVibes().fontFamily,
                           fontSize: 20,
-                          color: Color.fromARGB(255, 101, 100, 100)),
+                          color: const Color.fromARGB(255, 101, 100, 100)),
                       'Thank you for joining our special day. We would love to see all the beautiful moments you have captured!'),
                 ),
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.6),
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 50,
               ),
               ElevatedButton(
-                  onPressed: null,
+                  onPressed: () => _pickFiles(),
                   child: Text(
                     'upload',
                     textAlign: TextAlign.center,
